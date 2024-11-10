8a554655dce04a27a73dc3fdaedb810beba044de
Talha <talha@Talhas-MacBook-Pro.local>
2024-11-11 03:22:22 +0500

    update

--- admin/src/app/components/login/index.tsx
+++ admin/src/app/components/login/index.tsx
@@ -6,7 +6,7 @@ const Login = () => {
     <>
       <div className="relative overflow-hidden h-screen bg-muted dark:bg-dark">
         <div className="flex h-full justify-center items-center px-4">
-          <div className="rounded-lg dark:shadow-dark-md shadow-md bg-white dark:bg-darkgray p-6 relative w-full break-words md:w-[450px] w-full border-none ">
+          <div className="rounded-lg dark:shadow-dark-md shadow-md bg-white dark:bg-darkgray p-6 relative break-words md:w-[450px] w-full border-none ">
             <div className="flex h-full flex-col justify-center gap-2 p-0 w-full">
               <h2 className="text-2xl text-center text-dark my-3">
                 Admin Login
